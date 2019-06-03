089a83c22e14fe6368359c7a4d99f5f604af4df0
Noah Limes <limes.24@osu.edu>
2019-06-02 21:06:16 -0400

    quat_to_euler.py now works allowing for a simulated imu to interface with riptide control stack

--- riptide_simulation/src/quat_to_euler_imu.py
+++ riptide_simulation/src/quat_to_euler_imu.py
@@ -22,21 +22,11 @@ def callback(msg):
         z = msg.orientation.z
         eulerAnglesList = tf.transformations.euler_from_quaternion([w, x, y, z])
 
-        debugFile.write("eulerAnglesList\n")
-        debugFile.write("0: " + str(eulerAnglesList[0]) + '\n')
-        debugFile.write("1: " + str(eulerAnglesList[1]) + '\n')
-        debugFile.write("2: " + str(eulerAnglesList[2]) + '\n\n')
-
         eulerAngles_rad = Vector3()
         eulerAngles_rad.x = eulerAnglesList[0]
         eulerAngles_rad.y = eulerAnglesList[1]
         eulerAngles_rad.z = eulerAnglesList[2]
         real_topic = RealImu()
-
-        debugFile.write("eulerAngles_rad\n")
-        debugFile.write("x: " + str(eulerAngles_rad.x) + '\n')
-        debugFile.write("y: " + str(eulerAngles_rad.y) + '\n')
-        debugFile.write("z: " + str(eulerAngles_rad.z) + '\n\n')
 
         real_topic.header = msg.header
         # Gyroscope Euler angles roll, pitch, yaw, and status
@@ -47,15 +37,13 @@ def callback(msg):
         # Linear accelerations along x,y,z axes, and status
         real_topic.linear_acceleration = msg.linear_acceleration #X, Y, and Z axes in [m/s^2]
         # Angular rates along x,y,z axes, and status
-        real_topic.angular_velocity = angular_velocity #X, Y, and Z axes in [radians/s]
-        pub(real_topic)
+        real_topic.angular_velocity = msg.angular_velocity #X, Y, and Z axes in [radians/s]
+        
+        pub.publish(real_topic)
 
 def main():
-        global debugFile
-        debugFile = open("debug.txt","w") 
-        debugFile.seek(0)
-        debugFile.truncate()
         global cfg
+        global pub
         with open(config_path, 'r') as stream:
                 cfg = yaml.load(stream)
         sim_topic = cfg['sim_topic']
@@ -63,7 +51,6 @@ def main():
         sub = rospy.Subscriber(sim_topic, SimImu, callback)
         pub = rospy.Publisher(real_topic, RealImu, queue_size=10)
         rospy.spin()
-        debugFile.close()
 
 if __name__ == "__main__":
         rospy.init_node("quat_to_euler_imu")
