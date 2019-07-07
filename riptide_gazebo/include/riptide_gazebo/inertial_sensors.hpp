28b30b51bcf45f78ce7de9513cf0b0bfcf59e4c6
Parth Parekh <pparekh@houstonmechatronics.com>
2019-07-06 22:38:45 -0500

    Got all inertial sensor topics published correctly

--- riptide_gazebo/include/riptide_gazebo/inertial_sensors.hpp
+++ riptide_gazebo/include/riptide_gazebo/inertial_sensors.hpp
@@ -1,50 +1,48 @@
 #ifndef INERTIAL_SENSORS
 #define INERTIAL_SENSORS
 
-#include "eigen3/Eigen/Dense"
-#include "eigen3/Eigen/Core"
-#include "auv_core/math_lib.hpp"
-#include "auv_core/constants.hpp"
 #include <tf/tf.h>
+#include "auv_core/math_lib.hpp"
+#include "eigen3/Eigen/Core"
+#include "eigen3/Eigen/Dense"
 #include "eigen_conversions/eigen_msg.h"
 #include "math.h"
 
-#include "ros/ros.h"
-#include "gazebo_msgs/ModelStates.h"
-#include "riptide_msgs/Imu.h"
-#include "riptide_msgs/Depth.h"
-#include "nortek_dvl/Dvl.h"
 #include "auv_msgs/SixDoF.h"
+#include "gazebo_msgs/ModelStates.h"
+#include "nortek_dvl/Dvl.h"
+#include "riptide_msgs/Depth.h"
+#include "riptide_msgs/Imu.h"
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
 
 namespace riptide_gazebo
 {
 class InertialSensors
 {
- private:
-   ros::NodeHandle nh_;
-   ros::Subscriber modelStatesSub_, clockSub_;
-   ros::Publisher sixdofPub_, imuPub_, dvlPub_, depthPub_;
-   std::string modelName_, modelStatesTopic_;
-   bool modelFound_;
-   int modelIndex_;
-   double dt;
-   ros::Time lastClockTime_;
+private:
+  ros::NodeHandle nh_;
+  ros::Subscriber modelStatesSub_, uuvImuSub_;
+  ros::Publisher sixdofPub_, imuPub_, dvlPub_, depthPub_;
+  std::string modelName_, modelStatesTopic_;
+  bool modelFound_;
+  int modelIndex_;
 
-   riptide_msgs::Imu imuMsg_;
-   riptide_msgs::Depth depthMsg_;
-   nortek_dvl::Dvl dvlMsg_;
-   auv_msgs::SixDoF sixdofMsg_;
-   
-   Eigen::Vector3d linearVel_, lastLinearVel_, linearAccel_, angularVel_, rpy_;
-   Eigen::Quaterniond quatNED_, quatENU_;
-   bool firstIter_;
+  riptide_msgs::Imu imuMsg_;
+  riptide_msgs::Depth depthMsg_;
+  nortek_dvl::Dvl dvlMsg_;
+  auv_msgs::SixDoF sixdofMsg_;
 
- public:
-   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Eigen::Vector3d gravityNED_, linearVel_, linearAccel_, linearAccelIn_, angularVel_, rpy_;
+  Eigen::Quaterniond quatNED_, quatENU_, quatBodyFixedENU2NED_;
 
-   InertialSensors(ros::NodeHandle &nh);
-   void modelStatesCB(const gazebo_msgs::ModelStates::ConstPtr &msg);
-   Eigen::Quaterniond getNEDQuaterionFromENU(Eigen::Quaterniond quatENU);
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  InertialSensors(ros::NodeHandle &nh);
+  void modelStatesCB(const gazebo_msgs::ModelStates::ConstPtr &msg);
+  void uuvImuCB(const sensor_msgs::Imu::ConstPtr &msg);
+  Eigen::Quaterniond getNEDQuaterionFromENU(Eigen::Quaterniond quatENU);
 };
 }
 
--- riptide_gazebo/src/inertial_sensors.cpp
+++ riptide_gazebo/src/inertial_sensors.cpp
@@ -4,153 +4,167 @@ namespace riptide_gazebo
 {
 InertialSensors::InertialSensors(ros::NodeHandle &nh)
 {
-   nh_ = nh;
+  nh_ = nh;
 
-   std::string sixdofTopic, imuTopic, dvlTopic, depthTopic;
-   nh_.param("model", modelName_, std::string("maelstrom"));
-   nh_.param("model_states_topic", modelStatesTopic_, std::string("/gazebo/model_states"));
-   nh_.param("six_dof_topic", sixdofTopic, std::string("/auv_gnc/trans_ekf/six_dof"));
-   nh_.param("imu_topic", imuTopic, std::string("/state/imu"));
-   nh_.param("dvl_topic", dvlTopic, std::string("/state/dvl"));
-   nh_.param("depth_topic", depthTopic, std::string("/state/depth"));
+  std::string uuvImuTopic, sixdofTopic, imuTopic, dvlTopic, depthTopic;
+  nh_.param("model", modelName_, std::string("maelstrom"));
+  nh_.param("model_states_topic", modelStatesTopic_, std::string("/gazebo/model_states"));
+  nh_.param("uuv_imu_topic", uuvImuTopic, std::string("/sensors/imu"));
+  nh_.param("six_dof_topic", sixdofTopic, std::string("/auv_gnc/trans_ekf/six_dof"));
+  nh_.param("imu_topic", imuTopic, std::string("/state/imu"));
+  nh_.param("dvl_topic", dvlTopic, std::string("/state/dvl"));
+  nh_.param("depth_topic", depthTopic, std::string("/state/depth"));
 
-   modelStatesSub_ = nh_.subscribe<gazebo_msgs::ModelStates>(modelStatesTopic_, 1, &InertialSensors::modelStatesCB, this);
-   sixdofPub_ = nh_.advertise<auv_msgs::SixDoF>(sixdofTopic, 1);
-   imuPub_ = nh_.advertise<riptide_msgs::Imu>(imuTopic, 1);
-   dvlPub_ = nh_.advertise<nortek_dvl::Dvl>(dvlTopic, 1);
-   depthPub_ = nh_.advertise<riptide_msgs::Depth>(depthTopic, 1);
+  modelStatesSub_ = nh_.subscribe<gazebo_msgs::ModelStates>(modelStatesTopic_, 1, &InertialSensors::modelStatesCB, this);
+  uuvImuSub_ = nh_.subscribe<sensor_msgs::Imu>(uuvImuTopic, 1, &InertialSensors::uuvImuCB, this);
+  
+  sixdofPub_ = nh_.advertise<auv_msgs::SixDoF>(sixdofTopic, 1);
+  imuPub_ = nh_.advertise<riptide_msgs::Imu>(imuTopic, 1);
+  dvlPub_ = nh_.advertise<nortek_dvl::Dvl>(dvlTopic, 1);
+  depthPub_ = nh_.advertise<riptide_msgs::Depth>(depthTopic, 1);
 
-   linearVel_.setZero();
-   lastLinearVel_.setZero();
-   linearAccel_.setZero();
-   angularVel_.setZero();
-   rpy_.setZero();
+  linearVel_.setZero();
+  linearAccel_.setZero();
+  linearAccelIn_.setZero();
+  angularVel_.setZero();
+  rpy_.setZero();
 
-   quatNED_.w() = 1;
-   quatNED_.x() = 0;
-   quatNED_.y() = 0;
-   quatNED_.z() = 0;
+  quatNED_.w() = 1;
+  quatNED_.x() = 0;
+  quatNED_.y() = 0;
+  quatNED_.z() = 0;
 
-   quatENU_.w() = 1;
-   quatENU_.x() = 0;
-   quatENU_.y() = 0;
-   quatENU_.z() = 0;
+  quatENU_.w() = 1;
+  quatENU_.x() = 0;
+  quatENU_.y() = 0;
+  quatENU_.z() = 0;
 
-   modelFound_ = false;
-   modelIndex_ = 0;
-   firstIter_ = true;
+  quatBodyFixedENU2NED_ = auv_core::math_lib::toQuaternion(0, 0, M_PI);  // Roll by 180 deg
 
-   ROS_INFO("Inertial Sensors initialized");
+  gravityNED_.setZero();
+  gravityNED_(2) = 9.81;
+
+  modelFound_ = false;
+  modelIndex_ = 0;
+
+  ROS_INFO("Inertial Sensors initialized");
 }
 
 /**
- * This callback reads in IMU data from gazebo configured with the ENU frame, and then expresses all
- * of the values using an NED-measuring IMU (just like the LORD MicroStrain IMU)
- * NOTE: This code will ONLY work if the IMU used in gazebo is set to the 'world' reference frame. 
- * DO NOT use 'world_ned'
+ * \brief This callback reads in the model state from gazebo and publishes the appropriate sensor data on their topics
  */
 void InertialSensors::modelStatesCB(const gazebo_msgs::ModelStates::ConstPtr &msg)
 {
-   // Find index for modelName (look up every time in case Gazebo is shutdown then)
-   modelFound_ = false;
-   for (int i = 0; i < msg->name.size(); i++)
-   {
+  // Find index for modelName
+  if (!modelFound_)
+  {
+    for (int i = 0; i < msg->name.size(); i++)
+    {
       if (msg->name[i] == modelName_)
       {
-         modelFound_ = true;
-         modelIndex_ = i;
+        modelFound_ = true;
+        modelIndex_ = i;
       }
-   }
+    }
 
-   if (!modelFound_)
-   {
-      ROS_WARN("InertialSensors: Model Name %s not found in %s", modelName_.c_str(), modelStatesTopic_.c_str());
+    if (!modelFound_)
+    {
+      ROS_WARN("InertialSensors: Model Name %s not found in topic %s", modelName_.c_str(), modelStatesTopic_.c_str());
       return;
-   }
-   
-   ros::Time clockTime = ros::Time::now();
+    }
+  }
 
-   // Imu and DVL Math
-   tf::quaternionMsgToEigen(msg->pose[modelIndex_].orientation, quatENU_);
-   quatNED_ = InertialSensors::getNEDQuaterionFromENU(quatENU_);
-   rpy_ = auv_core::math_lib::toEulerAngle(quatNED_);
+  ros::Time clockTime = ros::Time::now();
 
-   tf::vectorMsgToEigen(msg->twist[modelIndex_].linear, linearVel_);
-   tf::vectorMsgToEigen(msg->twist[modelIndex_].angular, angularVel_);
-   if (firstIter_)
-   {
-      firstIter_ = false;
-   }
-   else
-   {
-      dt = clockTime.toSec() - lastClockTime_.toSec();
-      linearAccel_ = (linearVel_ = lastLinearVel_) / dt;
-   }
+  // Get NED quaternion and Euler Angles
+  tf::quaternionMsgToEigen(msg->pose[modelIndex_].orientation, quatENU_);
+  quatNED_ = InertialSensors::getNEDQuaterionFromENU(quatENU_);
+  rpy_ = auv_core::math_lib::toEulerAngle(quatNED_);
 
-   linearAccel_ = quatENU_.conjugate() * linearAccel_;
-   linearVel_ = quatENU_.conjugate() * linearVel_;
-   angularVel_ = quatENU_.conjugate() * angularVel_;
+  // Express velocity vectors in body-fixed NED frame
+  tf::vectorMsgToEigen(msg->twist[modelIndex_].linear, linearVel_);
+  tf::vectorMsgToEigen(msg->twist[modelIndex_].angular, angularVel_);
+  linearVel_ = quatENU_.conjugate() * linearVel_;     // Express in body-fixed ENU frame
+  angularVel_ = quatENU_.conjugate() * angularVel_;   // Express in body-fixed ENU frame
+  linearVel_ = quatBodyFixedENU2NED_ * linearVel_;    // Express in body-fixed NED frame
+  angularVel_ = quatBodyFixedENU2NED_ * angularVel_;  // Express in body-fixed NED frame
 
-   // SixDof Message
-   sixdofMsg_.header.stamp = clockTime;
-   sixdofMsg_.pose.position.x = msg->pose[modelIndex_].position.y;
-   sixdofMsg_.pose.position.y = msg->pose[modelIndex_].position.x;
-   sixdofMsg_.pose.position.z = -msg->pose[modelIndex_].position.z;
-   tf::quaternionEigenToMsg(quatNED_, sixdofMsg_.pose.orientation);
-   tf::vectorEigenToMsg(linearVel_, sixdofMsg_.velocity.linear);
-   tf::vectorEigenToMsg(angularVel_, sixdofMsg_.velocity.angular);
-   tf::vectorEigenToMsg(linearAccel_, sixdofMsg_.linear_accel);
-   
-   // IMU Message
-   imuMsg_.header.stamp = clockTime;
-   tf::vectorEigenToMsg(rpy_, imuMsg_.rpy_rad);
-   tf::vectorEigenToMsg(angularVel_, imuMsg_.ang_vel_rad);
+  // SixDof Message
+  sixdofMsg_.header.stamp = clockTime;
+  sixdofMsg_.pose.position.x = msg->pose[modelIndex_].position.y;
+  sixdofMsg_.pose.position.y = msg->pose[modelIndex_].position.x;
+  sixdofMsg_.pose.position.z = -msg->pose[modelIndex_].position.z;
+  tf::quaternionEigenToMsg(quatNED_, sixdofMsg_.pose.orientation);
+  tf::vectorEigenToMsg(linearVel_, sixdofMsg_.velocity.linear);
+  tf::vectorEigenToMsg(angularVel_, sixdofMsg_.velocity.angular);
+  tf::vectorEigenToMsg(linearAccel_, sixdofMsg_.linear_accel);
 
-   rpy_ = rpy_* 180 / M_PI;
-   angularVel_ = angularVel_* 180 / M_PI;
+  // IMU Message
+  imuMsg_.header.stamp = clockTime;
+  tf::quaternionEigenToMsg(quatNED_, imuMsg_.quaternion);
+  tf::vectorEigenToMsg(rpy_, imuMsg_.rpy_rad);
+  tf::vectorEigenToMsg(rpy_ * 180 / M_PI, imuMsg_.rpy_deg);
+  tf::vectorEigenToMsg(angularVel_, imuMsg_.ang_vel_rad);
+  tf::vectorEigenToMsg(angularVel_ * 180 / M_PI, imuMsg_.ang_vel_deg);
+  tf::vectorEigenToMsg(linearAccel_, imuMsg_.linear_accel);
+  imuMsg_.heading_alt = rpy_(2) * 180 / M_PI;
+  imuMsg_.heading_LORD = rpy_(2) * 180 / M_PI;
 
-   tf::vectorEigenToMsg(rpy_, imuMsg_.rpy_deg);
-   tf::vectorEigenToMsg(angularVel_, imuMsg_.ang_vel_deg);
-   tf::vectorEigenToMsg(linearAccel_, imuMsg_.linear_accel);
-   imuMsg_.heading_alt = rpy_(2);
-   imuMsg_.heading_LORD = rpy_(2);
+  // DVL Message
+  dvlMsg_.header.stamp = clockTime;
+  tf::vectorEigenToMsg(linearVel_, dvlMsg_.velocity);
 
-   // DVL Message
-   dvlMsg_.header.stamp = clockTime;
-   tf::vectorEigenToMsg(linearVel_, dvlMsg_.velocity);
+  // Depth Message
+  depthMsg_.header.stamp = clockTime;
+  depthMsg_.depth = -msg->pose[modelIndex_].position.z;
 
-   // Depth Message
-   depthMsg_.header.stamp = clockTime;
-   depthMsg_.depth = -msg->pose[modelIndex_].position.z;
-
-   sixdofPub_.publish(sixdofMsg_);
-   imuPub_.publish(imuMsg_);
-   dvlPub_.publish(dvlMsg_);
-   depthPub_.publish(depthMsg_);
-
-   lastLinearVel_ = linearVel_;
-   lastClockTime_ = clockTime;
+  sixdofPub_.publish(sixdofMsg_);
+  imuPub_.publish(imuMsg_);
+  dvlPub_.publish(dvlMsg_);
+  depthPub_.publish(depthMsg_);
 }
 
 /**
- * @param quatENU quaternion from Local ENU frame to the body-fixed ENU frame
+ * \brief Express the vehicle's linear accel in the body-fixed NED frame and remove the gravitational components.
+ * NOTE: This will only work if the IMU is set to use the 'world' reference frame. Do NOT use 'world_ned'
+ */
+void InertialSensors::uuvImuCB(const sensor_msgs::Imu::ConstPtr &msg)
+{
+  if (!modelFound_)
+    return;
+
+  // Express linear accel in body-fixed NED frame
+  linearAccelIn_(0) = -msg->linear_acceleration.x;  // For some reason with UUV Sim, have to negate the x-component
+  linearAccelIn_(1) = msg->linear_acceleration.y;
+  linearAccelIn_(2) = msg->linear_acceleration.z;
+
+  linearAccel_ = linearAccelIn_ - quatNED_.conjugate() * gravityNED_;
+  tf::vectorEigenToMsg(linearAccel_, imuMsg_.linear_accel);
+  std::cout << "rpy NED:" << std::endl << rpy_ << std::endl;
+  std::cout << "gravity in body frame NED:" << std::endl << quatNED_.conjugate() * gravityNED_ << std::endl;
+  std::cout << "accel in:" << std::endl << linearAccelIn_ << std::endl;
+  std::cout << "final accel:" << std::endl << linearAccel_ << std::endl;
+}
+
+/**
+ * @param quatENU Quaternion from Local ENU frame to the body-fixed ENU frame
  * \brief Returns the quaternion from local NED frame to the body-fixed NED frame
  */
 Eigen::Quaterniond InertialSensors::getNEDQuaterionFromENU(Eigen::Quaterniond quatENU)
 {
-   // Quaternion rotated 90 deg in yaw
-   Eigen::Quaterniond quat1 = auv_core::math_lib::toQuaternion(M_PI/2.0, 0, 0);
+  // Quaternion rotated 90 deg in yaw
+  Eigen::Quaterniond quat1 = auv_core::math_lib::toQuaternion(M_PI / 2.0, 0, 0);
 
-   // Quaternion difference from quat1 to quatENU
-   Eigen::Quaterniond qDiff = quat1.conjugate() * quatENU;
-   Eigen::Vector4d angleAxis1 = auv_core::math_lib::quaternion2AngleAxis(qDiff); // Angle Axis format
+  // Quaternion difference from quat1 to quatENU
+  Eigen::Quaterniond qDiff = quat1.conjugate() * quatENU;
+  Eigen::Vector4d angleAxis1 = auv_core::math_lib::quaternion2AngleAxis(qDiff);  // Angle Axis format
 
-   Eigen::Vector4d angleAxis2 = Eigen::Vector4d::Zero();
-   angleAxis2(0) = angleAxis1(0); // Angle
-   angleAxis2(1) = angleAxis1(1); // X component remains the same
-   angleAxis2(2) = -angleAxis1(2); // Negate y compoennt
-   angleAxis2(3) = -angleAxis1(3); // Negate z component
+  Eigen::Vector4d angleAxis2 = Eigen::Vector4d::Zero();
+  angleAxis2(0) = angleAxis1(0);   // Angle remains unchanged
+  angleAxis2(1) = angleAxis1(1);   // X component remains unchanged
+  angleAxis2(2) = -angleAxis1(2);  // Negate y compoennt
+  angleAxis2(3) = -angleAxis1(3);  // Negate z component
 
-   return auv_core::math_lib::angleAxis2Quaternion(angleAxis2); // Convert to quaternion
+  return auv_core::math_lib::angleAxis2Quaternion(angleAxis2);  // Convert to quaternion
 }
 }
