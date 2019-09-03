7869b280e33dbc8773adc045e4b963e2cbb1d579
Ted Sender <tsender97@gmail.com>
2019-09-02 21:59:55 -0400

    Updated calls to auv_gnc

--- riptide_gazebo/include/riptide_gazebo/inertial_sensors.hpp
+++ riptide_gazebo/include/riptide_gazebo/inertial_sensors.hpp
@@ -2,7 +2,7 @@
 #define INERTIAL_SENSORS
 
 #include <tf/tf.h>
-#include "auv_core/math_lib.hpp"
+#include "auv_core/rot3d.hpp"
 #include "eigen3/Eigen/Core"
 #include "eigen3/Eigen/Dense"
 #include "eigen_conversions/eigen_msg.h"
@@ -21,29 +21,29 @@ namespace riptide_gazebo
 class InertialSensors
 {
 private:
-  ros::NodeHandle nh_;
-  ros::Subscriber modelStatesSub_, uuvImuSub_;
-  ros::Publisher sixdofPub_, imuPub_, dvlPub_, depthPub_;
-  std::string modelName_, modelStatesTopic_;
-  bool modelFound_;
-  int modelIndex_;
+   ros::NodeHandle nh_;
+   ros::Subscriber modelStatesSub_, uuvImuSub_;
+   ros::Publisher sixdofPub_, imuPub_, dvlPub_, depthPub_;
+   std::string modelName_, modelStatesTopic_;
+   bool modelFound_;
+   int modelIndex_;
 
-  riptide_msgs::Imu imuMsg_;
-  riptide_msgs::Depth depthMsg_;
-  nortek_dvl::Dvl dvlMsg_;
-  auv_msgs::SixDoF sixdofMsg_;
+   riptide_msgs::Imu imuMsg_;
+   riptide_msgs::Depth depthMsg_;
+   nortek_dvl::Dvl dvlMsg_;
+   auv_msgs::SixDoF sixdofMsg_;
 
-  Eigen::Vector3d gravityNED_, linearVel_, linearAccel_, linearAccelIn_, angularVel_, rpy_;
-  Eigen::Quaterniond quatNED_, quatENU_, quatBodyFixedENU2NED_;
+   Eigen::Vector3d gravityNED_, linearVel_, linearAccel_, linearAccelIn_, angularVel_, rpy_;
+   Eigen::Quaterniond quatNED_, quatENU_, quatBodyFixedENU2NED_;
 
 public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  InertialSensors(ros::NodeHandle &nh);
-  void modelStatesCB(const gazebo_msgs::ModelStates::ConstPtr &msg);
-  void uuvImuCB(const sensor_msgs::Imu::ConstPtr &msg);
-  Eigen::Quaterniond getNEDQuaterionFromENU(Eigen::Quaterniond quatENU);
+   InertialSensors(ros::NodeHandle &nh);
+   void modelStatesCB(const gazebo_msgs::ModelStates::ConstPtr &msg);
+   void uuvImuCB(const sensor_msgs::Imu::ConstPtr &msg);
+   Eigen::Quaterniond getNEDQuaterionFromENU(Eigen::Quaterniond quatENU);
 };
-}
+} // namespace riptide_gazebo
 
 #endif
