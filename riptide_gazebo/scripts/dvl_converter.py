8d3fad32760b398531715042bf7179a9576540c2
blaine141 <blaine141@gmail.com>
2019-11-15 14:06:17 -0800

    DVL might be working

--- riptide_gazebo/scripts/dvl_converter.py
+++ riptide_gazebo/scripts/dvl_converter.py
@@ -4,20 +4,39 @@ import rospy
 import sensor_msgs.msg
 import riptide_msgs.msg
 from geometry_msgs.msg import Vector3, Quaternion
-from nortek_dvl.msg import Dvl
+from riptide_msgs.msg import Dvl
 from gazebo_msgs.msg import ModelStates
 import math
-import tf
+from tf.transformations import quaternion_conjugate, quaternion_multiply
 
 class dvlConverter():
     def __init__(self):
         self.timer = rospy.Timer(rospy.Duration(1.0/8), self.getVelocity)
-        self.pub = rospy.Publisher("/dvl", Dvl, queue_size=1)
+        self.pub = rospy.Publisher("/state/dvl", Dvl, queue_size=1)
 
     def getVelocity(self, event):
+        # Get state of the vehicle
         msg = rospy.wait_for_message("/gazebo/model_states", ModelStates)
         index = msg.name.index("puddles")
+
+        # Find orientation and world frame velocity
         velocity = msg.twist[index].linear
+        velocity = [velocity.x, velocity.y, velocity.z, 0]
+        orientation = msg.pose[index].orientation
+        quat = quaternion_conjugate((orientation.x, orientation.y, orientation.z, orientation.w))
+
+        # Rotate to body frame
+        velocity = quaternion_multiply(
+            quaternion_multiply(quat, velocity), 
+            quaternion_conjugate(quat)
+        )[:3]
+
+        # Publish
+        outMsg = Dvl()
+        outMsg.velocity.x = velocity[0]
+        outMsg.velocity.y = -velocity[1]
+        outMsg.velocity.z = -velocity[2]
+        self.pub.publish(outMsg)
         pass
 
 
